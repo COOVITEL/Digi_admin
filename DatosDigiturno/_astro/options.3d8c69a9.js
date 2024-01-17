5af9964c2ef91cf4126641105e0ae57ed6db7117
ManuRodriguezC <estebanclimb@gmail.com>
2024-01-16 20:55:42 -0500

    Update

--- DatosDigiturno/_astro/options.3d8c69a9.js
+++
@@ -1,1 +0,0 @@
-const o={plugins:{legend:{display:!1,labels:{color:"white"}}},scales:{y:{ticks:{color:"white"}},x:{ticks:{color:"white"}}}};export{o};
--- DatosDigiturno/_astro/options.94e5676b.js
+++
@@ -1,1 +0,0 @@
-const s={plugins:{legend:{display:!1,labels:{color:"white"}}},scales:{y:{ticks:{color:"white"}},x:{ticks:{color:"white"}}}},o={plugins:{legend:{display:!1,labels:{color:"white"}}},scales:{y:{ticks:{color:"white"}},x:{ticks:{color:"white",font:{size:10}}}}};export{s as a,o};
